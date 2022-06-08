c64fcc6dc9a1332db02ff9e998a013e5d04f47b8
IanMutuma <ian97mutuma@gmail.com>
2022-06-08 21:19:27 +0300

    done assignment

--- index.js
+++ index.js
@@ -1,1 +1,36 @@
-// Code your solution in this file!
+function distanceFromHqInBlocks(num1, num2 = 42){
+    let distance = num1 - num2;
+
+    return Math.abs(distance);
+
+}
+console.log (distanceFromHqInBlocks (43))
+
+function distanceFromHqInFeet (num1){
+     let distanceF = distanceFromHqInBlocks(num1) * 264 ;
+     return Math.abs(distanceF );
+}
+console. log (distanceFromHqInFeet(50))
+
+function distanceTravelledInFeet (num1,num2){
+    let travFeet = (num1-num2) * 264 ;
+    return Math.abs(travFeet);
+
+}
+console.log (distanceTravelledInFeet(34, 50)) 
+
+function calculatesFarePrice (D){
+    if (D < 400){
+        return "free" ;
+    }
+    if (400 < D < 2000){
+        return "2 cent per foot";
+    }
+    if (2000 < D < 2500){
+        return "25 $ ";
+    }
+    if (D > 2500){
+        return "cannot travel that far"
+    }
+}
+console.log (calculatesFarePrice(2112))
